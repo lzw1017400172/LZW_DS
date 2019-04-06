76b824bf0502ea4dd01f91b0826c590efff0987e
lzw1017400172 <1017400172@qq.com>
2019-04-06 13:22:42 +0800

    sasa
    sszsasasa

---
+++ lzw-item/lzw-item-service/src/main/java/com/lzw/LzwItemServiceApplication.java
@@ -0,0 +1,15 @@
+package com.lzw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class LzwItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LzwItemServiceApplication.class, args);
+	}
+
+}
